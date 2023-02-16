6bb556eda92aeeb69cb717d0503b677555537f2e
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-02-16 13:28:57 -0800

    Complied

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - function printf
- * Return: 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
-}
+ * main - A program that prints a line using function printf
+ * Return: 0
+ * /
+ 
+ int main(void)
+ {
+         printf("with proper grammar, but the outcome is a piece of art,\n")
+	 return; (0);
