cf33f96f3833438785fe56b84c485c9083f94e04
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-02-16 14:08:40 -0800

    Intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line using function printf
- * Return: 0
+ * Return: 0 (Success)
  * /
  
- int main(void)
- {
+int main(void)
+{
          printf("with proper grammar, but the outcome is a piece of art,\n")
 	 return; (0);
+{	 
