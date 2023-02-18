1090fb3d21f8a6a92ef4896112311b0a1f9ab7fb
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-02-18 07:37:27 -0800

    Size matters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - a program that prints the sizes of types on the computer it is compiled and run on
  * Return: 0 (Success)
@@ -11,10 +12,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)size of (a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)size of (b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of (c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of (d));
-printf("Size of a float: %lu byte (s)\n", (unsigned long)size of (f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(d));
+printf("Size of a float: %lu byte (s)\n", (unsigned long)size of(f));
 return (0);
 }
