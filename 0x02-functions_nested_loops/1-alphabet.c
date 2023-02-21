2a506c760510a1ed69163e6b456c8e49cd462369
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-02-21 14:22:21 -0800

    Alphabetize

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet printing function
+ *
+ * Return: always
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	       _putchar(x);
+	_putchar('\n');
+}
