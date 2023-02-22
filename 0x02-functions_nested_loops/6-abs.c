1d49599d62f383b55223e3018e3be2ecdf63995a
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-02-21 16:58:28 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ *
+ * @a: parameter to be chacked
+ *
+ * Return: always a
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
