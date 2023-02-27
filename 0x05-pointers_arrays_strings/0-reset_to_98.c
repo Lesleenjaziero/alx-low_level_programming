525c930edd9aa69df30466e1a5f8bf48938a749f
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-02-27 13:32:15 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to as parameter
+ *
+ * @n: given parameter
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
