36a2c033fd8789ad8eea92bbb8c1d4b171e2472d
Lesleenjaziero <lesleenjaziero@gmail.com>
2023-06-22 15:00:06 -0700

    That is the question?

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,29 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	int i = 0;
+	char *str, *sep = "";
 
-    for (const char *p = format; *p; p++)
-    {
-        switch (*p)
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                printf("%s", va_arg(args, char *));
-                break;
-            default:
-                break;
-        }
-    }
+	va_list list;
 
-    va_end(args);
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
 }
